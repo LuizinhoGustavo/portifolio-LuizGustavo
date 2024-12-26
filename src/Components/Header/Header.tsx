7ba8ac11311c54d31c081473b32866e330b3366f
lgsilva5 <lgsilva5@santoandre.sp.gov.br>
2024-12-26 10:05:44 -0300

    Corrections in the deployment

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
 
     return (
         <div className="w-full px-5 py-3 h-20 flex gap-3 mb-20 lg:mb-0">
-            <a href={`/Home`} className="outline-none"><div className="h-10 w-10 border-0 rounded-full bg-black flex justify-center"><House color="white" className='my-auto' /></div></a>
+            <a href={`/`} className="outline-none"><div className="h-10 w-10 border-0 rounded-full bg-black flex justify-center"><House color="white" className='my-auto' /></div></a>
 
             <a href={`/Projects`} className="outline-none"><div className="h-10 w-10 border-0 rounded-full bg-dark-blue flex justify-center"><FileJson2 color="white" className='my-auto' /></div></a>
 
--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import { Certificates } from './routes/Certificates.tsx';
 
 const router = createBrowserRouter([
   {
-    path: "/Home",
+    path: "",
     element: <App />,
   },
   {
